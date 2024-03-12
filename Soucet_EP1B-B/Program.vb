a8e6801c704d009c59a0217c9b97d062049d4ff5
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-03-12 09:34:14 +0100

    hotový koncept zadávání čísla

--- Soucet_EP1B-B/Program.vb
+++ Soucet_EP1B-B/Program.vb
@@ -2,16 +2,29 @@
 
 Module Program
     Sub Main(args As String())
-        Console.CursorVisible = False
+        'Console.CursorVisible = False
         Dim soucet, cislo As Integer
         Dim vstup As String
 
         Nadpis("Součet čísel", 40, 5, ConsoleColor.Black, ConsoleColor.White)
-        Nadpis("Zadej číslo:     ", 39, 10, ConsoleColor.Black, ConsoleColor.White)
         Nadpis($"Součet je {soucet}", 40, 15, ConsoleColor.Black, ConsoleColor.White)
-
+        Nadpis("Zadej číslo:     ", 39, 10, ConsoleColor.Black, ConsoleColor.White)
+        cislo = Zadavani(55, 11, ConsoleColor.White, ConsoleColor.Black)
         Console.ReadKey()
     End Sub
+
+    Function Zadavani(left As Integer, top As Integer, textColor As ConsoleColor, backColor As ConsoleColor) As Integer
+        Dim tempBackColor As ConsoleColor = Console.BackgroundColor
+        Dim tempForeColor As ConsoleColor = Console.ForegroundColor
+
+        Console.BackgroundColor = backColor
+        Console.ForegroundColor = textColor
+        Console.SetCursorPosition(left, top)
+        Console.Write("    ")
+        Console.SetCursorPosition(left + 3, top)
+        Console.BackgroundColor = tempBackColor
+        Console.ForegroundColor = tempForeColor
+    End Function
 
     Sub Nadpis(text As String, left As Integer, top As Integer, textColor As ConsoleColor, backColor As ConsoleColor)
         Dim tempBackColor As ConsoleColor = Console.BackgroundColor
