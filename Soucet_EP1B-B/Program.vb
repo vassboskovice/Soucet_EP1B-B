dc8aec21805b580a12a6bf762d3bef31889f62a3
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-03-19 09:40:50 +0100

    hotové sčítání

--- Soucet_EP1B-B/Program.vb
+++ Soucet_EP1B-B/Program.vb
@@ -5,11 +5,14 @@ Module Program
         'Console.CursorVisible = False
         Dim soucet, cislo As Integer
         Dim vstup As String
-
-        Nadpis("Součet čísel", 40, 5, ConsoleColor.Black, ConsoleColor.White)
-        Nadpis($"Součet je {soucet}", 40, 15, ConsoleColor.Black, ConsoleColor.White)
-        Nadpis("Zadej číslo:     ", 39, 10, ConsoleColor.Black, ConsoleColor.White)
-        cislo = Zadavani(55, 11, ConsoleColor.White, ConsoleColor.Black)
+        Do
+            Console.Clear()
+            Nadpis("Součet čísel", 40, 5, ConsoleColor.Black, ConsoleColor.White)
+            Nadpis($"Součet je {soucet}", 40, 15, ConsoleColor.Black, ConsoleColor.White)
+            Nadpis("Zadej číslo:     ", 39, 10, ConsoleColor.Black, ConsoleColor.White)
+            cislo = Zadavani(55, 11, ConsoleColor.White, ConsoleColor.Black)
+            soucet = soucet + cislo
+        Loop
         Console.ReadKey()
     End Sub
 
@@ -21,18 +24,21 @@ Module Program
         Console.ForegroundColor = textColor
         Console.SetCursorPosition(left, top)
         Console.Write("    ")
-        Console.SetCursorPosition(left + 3, top)
+        Console.SetCursorPosition(left, top)
+        Zadavani = Console.ReadLine
         Console.BackgroundColor = tempBackColor
         Console.ForegroundColor = tempForeColor
     End Function
 
     Sub Nadpis(text As String, left As Integer, top As Integer, textColor As ConsoleColor, backColor As ConsoleColor)
+        'zapamatování aktuální barvy pozadí a textu do proměnných
         Dim tempBackColor As ConsoleColor = Console.BackgroundColor
         Dim tempForeColor As ConsoleColor = Console.ForegroundColor
+        'vytvoření čáry, která je stejně dlouhá jako text v parametru text
         Dim line As String = New String("─", text.Length)
 
-        Console.BackgroundColor = backColor
-        Console.ForegroundColor = textColor
+        Console.BackgroundColor = backColor      'nastavení barvy pozadí podle parametru
+        Console.ForegroundColor = textColor      'další komentář popisující o co jde ...
         Console.SetCursorPosition(left, top)
         Console.Write($"┌──{line}──┐")
         Console.SetCursorPosition(left, top + 1)
